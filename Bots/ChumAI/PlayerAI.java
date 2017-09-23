02083043e06c8f0947b33cb3c7953a5317e93312
Alex Chum <AlexChum@Alexs-MacBook-Air.local>
2017-09-23 17:11:07 -0400

    Additional Code

--- Bots/ChumAI/PlayerAI.java
+++ Bots/ChumAI/PlayerAI.java
@@ -16,8 +16,9 @@ public class PlayerAI {
 	Point position;
 	EnemyUnit enemy;
 	int distance;
-	ArrayList<EnemyUnit> empty = new ArrayList<EnemyUnit>();
-	Map<String> unitIDs;
+//	ArrayList<EnemyUnit> empty = new ArrayList<EnemyUnit>();
+	Map<String, ArrayList<Point>> unitIDs;
+	ArrayList<Point> enemyNest = null;
 	
     public PlayerAI() {
         // Any instantiation code goes here
@@ -38,29 +39,50 @@ public class PlayerAI {
         Grow, become stronger
         Take over the world */
     		turn = 20;
+    		if (world.getEnemyNestPositions().length == 1) {
+    			if (world.getFriendlyTiles().length <= world.getEnemyTiles().length) {
+    				enemyNest = new ArrayList<Point>();
+    				enemyNest.add(world.getEnemyNestPositions()[0]);
+    			} else {
+    				enemyNest = null;
+    			}
+    		};
         for (FriendlyUnit unit: friendlyUnits) {
-        		if (turn < 20) { // CONQUER!
-        			
-        			
-        		} else { // DESTROY!!
+//        		if (turn < 20) { // CONQUER!
+//        			
+//        			
+//        		} else { // DESTROY!!
             		position = unit.getPosition();
-            		enemy = world.getClosestEnemyFrom(position, empty);
+            		enemy = world.getClosestEnemyFrom(position, null);
             		distance = world.getShortestPathDistance(position, enemy.getPosition());
             		if (distance == 1) {
             			world.move(unit, enemy.getPosition());
             		} else if (distance < 4) {
-    				if (!((enemy.getHealth()-4) > unit.getHealth() || (enemy.getHealth()+4) < unit.getHealth())) {
-    					world.move(unit, position);
-    				}
+            			// Tactic 1
+	    				if ((enemy.getHealth()+3) > unit.getHealth()) {
+	    					world.move(unit, position);
+	    				} else {
+	    					conquer (world, unit);
+	    				}
+            			// Tactic 2
+//	    				if (!((enemy.getHealth()-4) > unit.getHealth() || (enemy.getHealth()+4) < unit.getHealth())) {
+//	    					world.move(unit, position);
+//	    				} else {
+//	    					conquer (world, unit);
+//	    				}
 	    			} else {
-	    	            List<Point> path = world.getShortestPath(unit.getPosition(),
-	                            world.getClosestCapturableTileFrom(unit.getPosition(), null).getPosition(),
-	                            null);
-	    	            	if (path != null) world.move(unit, path.get(0));
+	    				conquer (world, unit);
 	    			}
-        		}
+//        		}
 
         }
+    }
+    
+    public void conquer (World world, FriendlyUnit unit) {
+        List<Point> path = world.getShortestPath(unit.getPosition(),
+                world.getClosestCapturableTileFrom(unit.getPosition(), null).getPosition(),
+                enemyNest);
+        	if (path != null) world.move(unit, path.get(0));
     }
     
 
