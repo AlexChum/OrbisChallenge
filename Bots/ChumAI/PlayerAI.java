817e18888b753ad8cf25f2bac99101bfee18b09e
Alex Chum <AlexChum@Alexs-MacBook-Air.local>
2017-09-24 12:22:10 -0400

    #13: Final Submission

--- Bots/ChumAI/PlayerAI.java
+++ Bots/ChumAI/PlayerAI.java
@@ -12,18 +12,24 @@ import java.util.*;
 
 public class PlayerAI {
     // Any field declarations go here
+	// Game Data
 	int turn = 0;
+	int tilesNum;
+	// To avoid reinitialization of the variable through everyloop
+	boolean dead;
+	String uid;
 	Point position;
 	EnemyUnit enemy;
 	int distance;
-	int tiles;
-
-	ArrayList<Point> winningTiles = new ArrayList<Point>();
-	ArrayList<Point> avoidPoint = new ArrayList<Point>();
+	Map<Direction, Point> neighbours;
+	// ArrayLists of Points that shortestPath will try to avoid
+	ArrayList<Point> winningTiles = new ArrayList<Point>(); // The enemy's LAST nest to be avoided
+	ArrayList<Point> avoidPoint = new ArrayList<Point>(); // My own nests position + winningTiles
+	ArrayList<Point> toConquer = new ArrayList<Point>(); // Priority Nests
+	Map<String, Point> ConquerMap = new HashMap<String, Point>(); // Assigned priority Nests
 	
     public PlayerAI() {
         // Any instantiation code goes here
-
     }
 
     /**
@@ -40,9 +46,13 @@ public class PlayerAI {
         Grow, become stronger
         Take over the world */
     	
-    		// Tries to create a map of unclustered nests
+    		// Initializes a map of unclustered nests
+    		// Assigns the number of tiles on this map
     		if (turn == 0) {
+    			tilesNum = world.getTiles().length;
+    			
     			noClusters (world);
+    			findEasyNests (world);
     		} else if (turn == 20) { // Clears position of all nests
     			avoidPoint.clear();
     		}
@@ -53,17 +63,31 @@ public class PlayerAI {
     		//TODO: REMOVE BEFORE SUBMITTING
     		System.out.println(turn);
     		
-    		// Already in a winning position. Ensuring we have enough score before taking over
-    		// the last nest
+    		// Already in a winning position (Opponent only has 1 nest after 13 turns). 
+    		// Ensuring we have enough every score available before taking over the last nest
     		if (turn > 13 && world.getEnemyNestPositions().length == 1) {
 	    		lastNest (world);
     		} else {
     			winningTiles.clear();
     		}
     		
+    		// Looping through "directed" fireflies
+    		for (Map.Entry<String, Point> entry: ConquerMap.entrySet()) {
+    			dead = true;
+    			for (FriendlyUnit unit: friendlyUnits) {
+    				if (entry.getKey().equals(unit.getUuid())) {
+    					dead = false;
+    				}
+    			}
+    			if (dead) {
+    				toConquer.add(ConquerMap.remove(entry.getKey()));
+    			}
+    		}
+    		
     		//Looping through every unit
         for (FriendlyUnit unit: friendlyUnits) {
         		position = unit.getPosition();
+        		uid = unit.getUuid();
         		// Can optimize by choosing the "biggest enemy" or "killable" enemy later on
         		enemy = world.getClosestEnemyFrom(position, null);
         		distance = world.getShortestPathDistance(position, enemy.getPosition());
@@ -76,6 +100,18 @@ public class PlayerAI {
     				} else {
     					conquer (world, unit);
     				}
+    				// Implementation not working
+    			} else if (ConquerMap.containsKey(uid)) {
+    				conquer (world, unit, ConquerMap.get(uid));
+    			} else if (!toConquer.isEmpty()) {
+    				int shortestIndex = 1000;
+    				for (Point p: toConquer) {
+    					if (world.getShortestPathDistance(position, p)< shortestIndex) {
+    						shortestIndex = toConquer.indexOf(p);
+    					}
+    				}
+    				ConquerMap.put(uid, toConquer.remove(0));
+    				conquer (world, unit, ConquerMap.get(uid));
     			} else {
     				conquer (world, unit);
     			}
@@ -85,14 +121,27 @@ public class PlayerAI {
 //	    				} else {
 //	    					conquer (world, unit);
 //	    				}
-
         }
     }
     
+    /**
+     * This method is the original AI pattern with a some points to avoid in its Path
+     * 
+     * @param world The latest state of the world.
+     * @param unit The unit currently undergoing <doMove>
+     */
     public void conquer (World world, FriendlyUnit unit) {
-        List<Point> path = world.getShortestPath(unit.getPosition(),
-                world.getClosestCapturableTileFrom(unit.getPosition(), avoidPoint).getPosition(),
-                avoidPoint);
+    		List<Point> path = null;
+    		if (turn > 15 && winningTiles.isEmpty()) {
+	        path = world.getShortestPath(unit.getPosition(),
+	                world.getClosestEnemyNestFrom(unit.getPosition(), avoidPoint),
+	                avoidPoint);
+    		}
+        	if (path == null) {
+        		path = world.getShortestPath(unit.getPosition(),
+        				world.getClosestCapturableTileFrom(unit.getPosition(), avoidPoint).getPosition(), 
+        				avoidPoint);
+        	}
         	if (path == null) {
         		path = world.getShortestPath(unit.getPosition(),
         				world.getClosestCapturableTileFrom(unit.getPosition(), avoidPoint).getPosition(), 
@@ -101,17 +150,71 @@ public class PlayerAI {
         	if (path != null) world.move(unit, path.get(0));
     }
     
+    
+    public void conquer (World world, FriendlyUnit unit, Point destination) {
+        	List<Point> path = world.getShortestPath(unit.getPosition(), destination, avoidPoint);
+        	if (path == null) {
+        		if (ConquerMap.containsKey(unit.getUuid())) {
+            		ConquerMap.remove(unit.getUuid());
+        		}
+        		conquer (world, unit);
+        	} else {
+        		world.move(unit, path.get(0));
+        	}
+    }
+
+    		// Unimplemented Feature
+//    public void chooseEnemy (World world, FriendlyUnit unit, EnemyUnit[] enemy) {
+//		neighbours = world.getNeighbours(unit.getPosition());
+//		for (Map.Entry<Direction, Point> entry: neighbours.entrySet()) {
+//		}
+//    }
+    
+    public void findEasyNests (World world) {
+    		Point startNest = world.getFriendlyNestPositions()[0];
+    		int walls;
+    		List<Point> entries = new ArrayList<Point>();
+    		Point tile;
+    		for (int i=0; i<19; i++) {
+    			for (int j=0; j<19; j++) {
+    				tile = new Point(i,j);
+	    			neighbours = world.getNeighbours(tile);
+	    			walls = 0;
+	    			entries.clear();
+	    			for (Map.Entry<Direction, Point> entry: neighbours.entrySet()) {
+	    				if (world.isWall(entry.getValue()))	{
+	    					walls++;
+	    				} else {
+	    					entries.add(entry.getValue());
+	    				}
+	    			}
+	    			if (walls > 1) {
+	    				if (world.getShortestPathDistance(startNest, tile) < 20) {
+	    					avoidPoint.add(tile);
+	    					for (Point p: entries) {
+	    						toConquer.add(p);
+	    					}
+	    				}
+	    			}
+    			}
+    	    	}
+    }
+    
+    /**
+     * This method adds to ArrayList<Point> the position of the last nest 
+     * such that the fireflies will avoid those Points.
+     * 
+     * @param world The latest state of the world.
+     */
     public void lastNest (World world) {
+    	
 		int permanentTiles = 1;
 		for (Tile tile: world.getEnemyTiles()) {
 			if (tile.isPermanentlyOwned())	permanentTiles++;
 		}
-		Map<Direction, Point> nest = world.getNeighbours(world.getEnemyNestPositions()[0]);
-		for (Map.Entry<Direction, Point> entry: nest.entrySet()) {
-			if (!world.isWall(entry.getValue()))	permanentTiles++;
-		}	
-		System.out.println(permanentTiles);
-		if (world.getFriendlyTiles().length == (tiles-permanentTiles) || turn > 90) {
+		permanentTiles += world.getEnemyTilesAround(world.getEnemyNestPositions()[0]).length;
+		
+		if (world.getFriendlyTiles().length == (tilesNum - permanentTiles) || turn > 90) {
 			avoidPoint.clear();
 			winningTiles.clear();
 		} else {
@@ -129,12 +232,21 @@ public class PlayerAI {
 		}
     }
     
-    public void noClusters (World world) {
-		tiles = world.getTiles().length;
+    /**
+     * This method adds to ArrayList<Point> the position of the nearest *defined* 
+     * unclustered nest positions such that the fireflies will avoid those Points.
+     * 
+     * There are the predefined paths for unclustered nests
+     * 
+     * @param world The latest state of the world.
+     */
+    public void noClusters (World world) {		
 		Point mainNest = world.getFriendlyNestPositions()[0];
 		int path_one, path_two, x, y;
 		path_one = path_two = 0;
 		// Brute force computation
+//		// Looped Computation does not seem to work because of world.getShortestPathDistance
+//		// It does not like references as arguments
 		path_one += world.getShortestPathDistance(mainNest, mainNest.add(new Point(2, 1)));
 		path_one += world.getShortestPathDistance(mainNest, mainNest.add(new Point(-1, 2)));
 		path_one += world.getShortestPathDistance(mainNest, mainNest.add(new Point(1, -2)));
@@ -152,14 +264,14 @@ public class PlayerAI {
 		path_two += world.getShortestPathDistance(mainNest, mainNest.add(new Point(-3, -1)));
 		path_two += world.getShortestPathDistance(mainNest, mainNest.add(new Point(1, -3)));
 		path_two += world.getShortestPathDistance(mainNest, mainNest.add(new Point(-1, 3)));
-//		// Automated Computation does not seem to work because of world.getShortestPathDistance
-//		// It does not like references as arguments
+		
+//		// Looped Computation
 //		mainNest = mainNest.add(new Point(1, -3));
 //		x = mainNest.getX();
 //		y = mainNest.getY();
 //		for (int i=0; i<3; i++) {
 //			for (int j=0; j<3; j++) {
-//				path_one += world.getShortestPathDistance(new Point(3, 2), new Point(3,3));
+//				path_one += world.getShortestPathDistance(mainNest, new Point(x+j, y+2*j));
 //			}
 //			x -= 2;
 //			y++;
@@ -171,11 +283,12 @@ public class PlayerAI {
 //		y = mainNest.getY();
 //		for (int i=0; i<3; i++) {
 //			for (int j=0; j<3; j++) {
-//				path_two += world.getShortestPathDistance(new Point(3, 2), new Point(x+2*j, y+j));
+//				path_two += world.getShortestPathDistance(mainNest, new Point(x+2*j, y+j));
 //			}
 //			x--;
 //			y += 2;
 //		}
+		
 		if (path_one > path_two) {
 			//Starting for 5x5 (2, -6)
 			//Starting for 7x7 (3, -9)
@@ -203,16 +316,6 @@ public class PlayerAI {
 				y += 2;
 			}
 		}
-    }
-    
-    public Point convertToMapPoint (Point point) {
-    		if (point.getX() < 0) {
-    			point.add(new Point(19, 0));
-    		}
-    		if (point.getY() < 0) {
-    			point.add(new Point(0, 19));
-    		}
-    		return point;
     }
 
 }
